bb1d1dc2492ee1b4fcca400d949239c9d7295284
Your Name <you@example.com>
2023-08-14 22:34:47 +0530

    updating the Auto Push

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import psutil
-from flask import Flask, render_template
+from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__ , static_folder='../static')
 
 @app.route("/")
 def index():
@@ -10,7 +10,7 @@ def index():
     Message = None
     if cpu_metric > 80 or mem_metric > 80:
         Message = "High CPU or Memory Detected, scale up!!!"
-    return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message=Message)
+    return ("CPU utilization: {cpu_metric} and Menory utilization: {mem_metric}")
 
 if __name__=='__main__':
     app.run(debug=True, host = '0.0.0.0')
